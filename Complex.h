d25112ca2c9d92539c1ea53b546f81b3b30ff2bd
klorin <45018060+klorin@users.noreply.github.com>
2019-01-06 08:28:00 +0200

    Update Complex.h

--- Complex.h
+++ Complex.h
@@ -1,13 +1,72 @@
 #ifndef EX3_COMPLEX_H
 #define EX3_COMPLEX_H
+
+
 namespace MtmMath {
     class Complex {
-        double re,im;
+        double re, im;
     public:
-        Complex(double r=0, double i=0);
-        Complex(const Complex&) ;
-        Complex& operator=(const Complex&);
+        explicit Complex(double r = 0, double i = 0) :
+                re(r), im(i) {}
+
+        ~Complex() = default;
+
+        Complex(const Complex &) = default;
+
+        Complex &operator=(const Complex &) = default;
+
+        Complex &operator+=(const Complex &c);
+
+        Complex &operator*=(const Complex &c);
+
+        Complex &operator-=(const Complex &c);
+
+        Complex operator-() const;
+
+        bool operator==(const Complex &c) const;
 
     };
+
+    MtmMath::Complex operator+(const Complex &a, const Complex &b);
+
+    MtmMath::Complex operator*(const Complex &a, const Complex &b);
+
+    MtmMath::Complex operator-(const Complex &a, const Complex &b);
+
+
+    MtmMath::Complex &Complex::operator+=(const Complex &c) {
+        re += c.re;
+        im += c.im;
+        return *this;
+    }
+
+    MtmMath::Complex &Complex::operator*=(const Complex &c) {
+        re *= c.re;
+        im *= c.im;
+        return *this;
+    }
+
+    MtmMath::Complex &Complex::operator-=(const Complex &c) {
+        return this->operator+=(-c); // or *this += -c
+    }
+
+    MtmMath::Complex Complex::operator-() const {
+        return Complex(-re, -im);
+    }
+
+    MtmMath::Complex operator+(const Complex &a, const Complex &b) {
+        Complex c = a;
+        return c += b;
+    }
+
+    MtmMath::Complex operator*(const Complex &a, const Complex &b) {
+        Complex c = a;
+        return c *= b;
+    }
+
+    bool MtmMath::Complex::operator==(const Complex &c) const {
+        return c.re == re && c.im == im;
+    }
+
 }
 #endif //EX3_COMPLEX_H
